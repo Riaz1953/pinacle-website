b365e38e78dbede77356ca4d6f26fef67f01c3f8
Md Riaz <fxkiyan2020@gmail.com>
2026-05-17 23:15:11 +0200

    Full Porfolio Website  Complete

--- src/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("bar-icon").addEventListener("click", () => {
-  const navbar = document.getElementById("navbar");
-  navbar.classList.toggle("left-[0px]");
-  console.log("buton clicked");
-});
