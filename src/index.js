4239e84b90d5fb803cee3819a60487f3c2c11c3a
Md Riaz <fxkiyan2020@gmail.com>
2026-05-17 12:10:20 +0200

    pinacle website complete

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.getElementById("bar-icon").addEventListener("click", () => {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("left-[0px]");
+  console.log("buton clicked");
+});
